f5ccaaa6f19e7d120b0a64544ab417ef37ebf32b
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-09-29 15:02:26 -0300

    implementado o metodo resgistrarProfessorTitular

--- src/DigitalHouseManager.kt
+++ src/DigitalHouseManager.kt
@@ -2,8 +2,8 @@ class DigitalHouseManager () {
 
 
     private var listaDeAlunos = ArrayList<Aluno>()
-    private var listaDeProfessores = ArrayList<Professor>()
-    var listaDeCursos = mutableListOf<Curso>()
+    var listaDeProfessores = ArrayList<Professor>()
+    private var listaDeCursos = mutableListOf<Curso>()
     private var listaDeMatriculas = ArrayList<Matricula>()
 
     fun registrarCurso(nome: String, codigoCurso: Int, qtdMaxDeAlunos: Int) {
@@ -44,4 +44,23 @@ class DigitalHouseManager () {
         listaDeProfessores.add(novoProfessor)
     }
 
+    fun registrarProfessorTitular (nome: String,
+                                      sobrenome: String,
+                                      codigoProfessor: Int,
+                                      especialidade: String) {
+
+        val novoProfessor = ProfessorTitular(nome,
+                sobrenome,
+                codigoProfessor,
+                especialidade)
+
+        listaDeProfessores.forEach {
+            if (novoProfessor.equals(it)){
+                throw Exception("Código do professor já esta em uso!")
+                return
+            }
+        }
+        listaDeProfessores.add(novoProfessor)
+
+    }
 }
