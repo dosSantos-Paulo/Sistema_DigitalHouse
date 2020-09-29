68e300c6f99366f6418c62a6fc43a7ac013074ad
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-09-29 14:20:11 -0300

    foi implementado o médoto registrarProfessorAdjunto

--- src/DigitalHouseManager.kt
+++ src/DigitalHouseManager.kt
@@ -2,27 +2,46 @@ class DigitalHouseManager () {
 
 
     private var listaDeAlunos = ArrayList<Aluno>()
-    private var listaDeProfessores = ArrayList<Professor>()
-    var listaDeCursos = mutableListOf<Curso>()
+    var listaDeProfessores = ArrayList<Professor>()
+    private var listaDeCursos = mutableListOf<Curso>()
     private var listaDeMatriculas = ArrayList<Matricula>()
 
-    fun registrarCurso(nome: String, codigoDoCurso: Int, qtdMaxDeAlunos: Int) {
+    fun registrarCurso(nome: String, codigoCurso: Int, qtdMaxDeAlunos: Int) {
         listaDeCursos.forEach {
-            if (codigoDoCurso.equals(it.codigoDoCurso)){
-                throw Exception("Código do curso já esta em uso, escolha outro código")
+            if (codigoCurso.equals(it.codigoDoCurso)){
+                throw Exception("Código do curso já esta em uso!")
                 return
             }
         }
-        listaDeCursos.add(Curso(nome, codigoDoCurso, qtdMaxDeAlunos))
+        listaDeCursos.add(Curso(nome, codigoCurso, qtdMaxDeAlunos))
         return
     }
 
-    fun excluirCurso(codigoDoCurso: Int) {
+    fun excluirCurso(codigoCurso: Int) {
         listaDeCursos.forEach {
-            if (codigoDoCurso.equals(it.codigoDoCurso)){
+            if (codigoCurso.equals(it.codigoDoCurso)){
                 listaDeCursos.remove(it)
             }
         }
+    }
+
+    fun registrarProfessorAdjunto(nome: String,
+                                  sobrenome: String,
+                                  codigoProfessor: Int,
+                                  quantidadeDeHoras: Int) {
+
+        val novoProfessor = ProfessorAdjunto(nome,
+                sobrenome,
+                codigoProfessor,
+                quantidadeDeHoras)
+
+        listaDeProfessores.forEach {
+            if (novoProfessor.equals(it)){
+                throw Exception("Código do professor já esta em uso!")
+                return
+            }
+        }
+        listaDeProfessores.add(novoProfessor)
     }
 
 }
