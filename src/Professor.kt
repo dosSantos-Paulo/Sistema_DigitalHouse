b16dbecde90c5173ddb24c79f20251eb37cf4ccc
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-10-01 19:10:22 -0300

    add atributo tempoDeCasa a classe Professor

--- src/Professor.kt
+++ src/Professor.kt
@@ -1,6 +1,10 @@
+import java.time.LocalDateTime
+
 abstract class Professor (protected open val nome: String,
                           protected open val sobrenome: String,
                           open val codigoDoProfessor: Int){
+
+    var tempoDeCasa = 0
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -16,7 +20,8 @@ abstract class Professor (protected open val nome: String,
     }
 
     override fun toString(): String {
-        return "Professor(nome='$nome', sobrenome='$sobrenome', codigoDoProfessor=$codigoDoProfessor)"
+        return "Professor(nome='$nome', sobrenome='$sobrenome', " +
+                "codigoDoProfessor=$codigoDoProfessor, tempoDeCasa=$tempoDeCasa)"
     }
 
 
